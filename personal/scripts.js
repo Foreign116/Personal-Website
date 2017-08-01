fc4c1358f7283408d4905582e00234f321f65808
Mehmet Ozdemir <whosforeign@gmail.com>
2017-08-01 16:58:59 -0400

    First Commit
    Commit

---
+++ personal/scripts.js
@@ -0,0 +1,9 @@
+	$("#gh").click(function() {
+		window.open("https://github.com/Foreign116","_blank");
+	});
+	$("#lin").click(function() {
+		window.open("https://www.linkedin.com/in/mcozdemi","_blank");
+	});
+	$("#res").click(function(){
+		window.open("resume.pdf","_blank");
+	});
